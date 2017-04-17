ab29821d619ffc601be9ca4412c8ec30b06bb76a
Firmansyah <taujago@gmail.com>
2017-04-18 05:32:43 +0800

    perbaikan grapic

--- application/modules/ex_grafik_lap/views/ex_grafik_lap_grafik.php
+++ application/modules/ex_grafik_lap/views/ex_grafik_lap_grafik.php
@@ -71,61 +71,70 @@
   $(document).ready(function() {
 
     $('#view').highcharts({
-        title: {
-            text: '<?php echo $title; ?>',
-            x: -20 //center
-        },
-    subtitle: {
-            text: 'Tahun : <?php echo date('Y'); ?>',
-            x: -20
-        },
-        xAxis: {
-            categories: [
-          
-          'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
         
-      ]
-        },
-        yAxis: {
-            title: {
-                text: '<?php echo $title.' Tahun : '.date('Y'); ?>'
-            },
-            plotLines: [{
-                value: 0,
-                width: 1,
-                color: '#f1c40f'
-            }]
-        },
-        tooltip: {
-            valueSuffix: ' Orang'
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle',
+chart: {
+        type: 'column'
+    },
+    title: {
+        text: '<?php echo $title; ?> '
+    },
+    subtitle: {
+        text: '<?php echo $title.' Tahun : '.$tahun; ?>'
+    },
+    xAxis: {
+        categories: [
+            'Januari',
+            'Februari',
+            'Maret',
+            'April',
+            'Mei',
+            'Juni',
+            'Juli',
+            'Agustus',
+            'September',
+            'Oktober',
+            'November',
+            'Desember'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Jumlah kasus'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y} Kasus</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
             borderWidth: 0
-        },
-        series: [{
-            name: 'Jumlah',
-            data: [
-        <?php
-          
-          if($query == null ) {
-            for($x=1; $x<=count($query); $x++){
-              echo '0, ';
-            }
-          } else {
-            $data = array('Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember');
-            for($x=0; $x<count($data); $x++) {
-              
-              echo $query->$data[$x].', ';
-            }
-          }
-          
+        }
+    },
+    series: [{
+        name: 'Jumlah ',
+        data: [
+        0,0,0,0,0,0,0,0,0,0,0
+       // 49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4
+        <?php 
+            // foreach($query as $index =>$vl):
+            //                 echo $vl.', ';
+            // endforeach;
         ?>
+        
         ]
-        }]
+
+            }]
+
     });
+
 
     $("#cari_button").click(function() {
 
