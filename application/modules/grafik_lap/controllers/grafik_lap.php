4b6abf683f4728509e745f9b777dfd5487c78c1d
Firmansyah <taujago@gmail.com>
2017-04-18 08:58:27 +0800

    graphic

--- application/modules/grafik_lap/controllers/grafik_lap.php
+++ application/modules/grafik_lap/controllers/grafik_lap.php
@@ -128,6 +128,7 @@ class grafik_lap extends admin_controller  {
 				  WHERE YEAR(tanggal) = ".$tahun;
 
 		$data_array['query'] = $this->db->query($query)->row_array();
+		//ada yagn dirubah loh ya 
 		$data_array['tahun'] = $tahun;
 		$data_array['title'] = $title;
 
