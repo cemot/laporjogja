e6702b592ae045325ec27655776e010640eb6c1f
Firmansyah <taujago@gmail.com>
2017-04-23 20:54:09 +0800

    Penambahan jumlah kapasitas lapas

--- application/modules/ex_diagram_pie/controllers/ex_diagram_pie.php
+++ application/modules/ex_diagram_pie/controllers/ex_diagram_pie.php
@@ -25,8 +25,19 @@ function index(){
 		$data_array=array();
         $userdata =  $_SESSION['userdata'];
 
+
+        $res = $this->db->get("m_lapas");
+        $arr_lapas = array();
+        foreach($res->result() as $row) : 
+        	$nama_lapas = str_replace(" ","",(strtolower($row->lapas)));
+        	$arr_lapas[$nama_lapas] = $row->kapasitas;
+    	endforeach;
+
+
+
         $query  = "SELECT asal_upt, COUNT(no_reg) jumlah FROM main group by asal_upt";
         // $jml    = $this->db->query($query)->result();
+        $data_array['arr_lapas'] = $arr_lapas;
         $data_array['jml'] = $this->db->query($query)->result_array();
         // show_array($data_array);
         // exit();
