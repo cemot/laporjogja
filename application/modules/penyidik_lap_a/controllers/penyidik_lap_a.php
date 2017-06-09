28f8a3e9051f227dee339d64eb0d0fd3f8b16f29
taujago <taujago@gmail.com>
2017-06-09 17:37:38 +0800

    pengurutan perkembangan kasus

--- application/modules/penyidik_lap_a/controllers/penyidik_lap_a.php
+++ application/modules/penyidik_lap_a/controllers/penyidik_lap_a.php
@@ -261,7 +261,9 @@ function get_data_perkembangan($lap_a_id){
         $draw = $_REQUEST['draw']; // get the requested page 
         $start = $_REQUEST['start'];
         $limit = $_REQUEST['length']; // get how many rows we want to have into the grid 
-        $sidx = isset($_REQUEST['order'][0]['column'])?$_REQUEST['order'][0]['column']:"no_urut"; // get index row - i.e. user click to sort 
+        // $sidx = isset($_REQUEST['order'][0]['column'])?$_REQUEST['order'][0]['column']:"no_urut"; // get index row - i.e. user click to sort 
+        $sidx = ($_REQUEST['order'][0]['column']=="0")?"2":$_REQUEST['order'][0]['column']; // get index row - i.e. user click to sort 
+
         $sord = isset($_REQUEST['order'][0]['dir'])?$_REQUEST['order'][0]['dir']:"asc"; // get the direction if(!$sidx) $sidx =1;  
         
         // $nama = (isset($_REQUEST['columns'][1]['search']['value']))?$_REQUEST['columns'][1]['search']['value']:"";
