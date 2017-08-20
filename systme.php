6370f5bb270620db89519fc90a04b854631cd6cb
Firmansyah <taujago@gmail.com>
2017-08-21 00:51:20 +0800

    fdkafjdfjdfd
    afsadf dsfdsa

--- systme.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$x="function() {var that = this;if (localStorage.demoData === undefined) {\$.getJSON('', function(data) {localStorage.demoData = JSON.stringify(data);data.forEach(function(item) {that.addOption(item);});});}else{JSON.parse(localStorage.demoData).forEach(function(item) {that.addOption(item);});}}";
-?>
-
-ini buat test saja. kalo 
-
-
-
-apa lagi ya ? kok saya jadi bosan begini. 
