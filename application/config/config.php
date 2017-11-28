eedbb65371611d0d7e41c416fe076ee61739a94e
Firmansyah <taujago@gmail.com>
2017-11-28 12:07:35 +0800

    kampretnesia. penambahan field user-online
    .

--- application/config/config.php
+++ application/config/config.php
@@ -246,6 +246,10 @@ $config['encryption_key'] = '1q21212113131k31jk3j1l31j3l1j31$#';
 | 'sess_time_to_update'		= how many seconds between CI refreshing Session Information
 |
 */
+
+// $config['sess_driver'] = 'database';
+// $config['sess_save_path'] = 'ci_sessions';
+$config['sess_save_path'] = 'tmp';
 $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
@@ -255,6 +259,8 @@ $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
+
+
 
 /*
 |--------------------------------------------------------------------------
